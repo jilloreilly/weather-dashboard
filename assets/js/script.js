6657abc67a6c5491500b63e0e7c0041264cbfc81
jillo <jill.l.oreilly@gmail.com>
2024-01-13 09:21:07 +0000

    Add _DS_Store to .gitignore

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,22 @@ function fetchWeather(search) {
         })
         .then(function (data) {
             console.log(data);
-            const currentWeather = data.list;
+            displayForecast(data);
+        })
+    })
+};
+
+function displayCurrentWeather(currentWeather) {
+  const iconUrl = `https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}.png` 
+  console.log(iconUrl)
+  const icon = $('#icon').attr('src', iconUrl);
+  const temp = $('#temp').text(`Temperature: ${currentWeather.main.temp} C`);
+  const wind = $('#wind').text(`Wind: ${currentWeather.wind.speed} kph`);
+  const humidity = $('#humidity').text(`Humidity: ${currentWeather.main.humidity} %`);
+}
+
+function displayForecast(data) {
+  const currentWeather = data.list;
             displayCurrentWeather(data.list[0]);
             
             const fiveDayForecast = currentWeather.filter(function (data) {
@@ -29,32 +44,29 @@ function fetchWeather(search) {
             });
            console.log(fiveDayForecast);
 
+           $('#forecast').empty();
+
            for (let i = 0; i < fiveDayForecast.length; i++) {
             const day = fiveDayForecast[i];
 
-            
+            const cardCol = $('<div>').attr('class', 'col-md');
+            const forecastCard = $('<div>').attr('class', 'card');
+            const forecastBody = $('<div>').attr('class', 'card-body');
+            const forecastIconUrl = `https://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
+            const forecastIcon = $('<img>').attr('src', forecastIconUrl);
+            const forecastTitle = $('<h5>').attr('class', 'card-title').text(dayjs(day.dt_txt).format('MM-DD-YYYY')); // Year optional
+            const forecastTemp = $('<p>').text(`Temp: ${day.main.temp} C`);
+            const forecastWind = $('<p>').text(`Wind: ${day.wind.speed} kph`);
+            const forecastHumidity = $('<p>').text(`Humidity: ${day.main.humidity} %`);
 
-          //   <div class="card">
-          //   <div class="card-body">
-          //     <h3 id="card-title"></h3>
-          //     <img id="icon">
-          //     <p id="temp"></p>
-          //     <p id="wind"></p>
-          //     <p id="humidity"></p>
-          //   </div>
-          // </div>
+            $('#forecast').append(cardCol);
+            cardCol.append(forecastCard);
+            forecastCard.append(forecastBody);
+            forecastBody.append(forecastTitle, forecastIcon, forecastTemp, forecastWind, forecastHumidity);
+
+         
             
            }
-        })
-    })
-};
-
-function displayCurrentWeather(currentWeather) {
-  const iconUrl = `https://openweathermap.org${currentWeather.weather.icon}.png` // TBC
-  const icon = $('#icon').attr('src', iconUrl);
-  const temp = $('#temp').text(`Temperature: ${currentWeather.main.temp} C`);
-  const wind = $('#wind').text(`Wind: ${currentWeather.wind.speed} kph`);
-  const humidity = $('#humidity').text(`Humidity: ${currentWeather.main.humidity} %`);
 }
 
     
@@ -70,3 +82,10 @@ function displayCurrentWeather(currentWeather) {
 
 
     })
+
+
+    // Movie button activity for persistant search (activity day 2 - 5 )
+
+    // Style
+
+    // Search 
