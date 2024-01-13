e2ea07fafc0eb13984ed5b2e9222d96baa91278d
jillo <jill.l.oreilly@gmail.com>
2024-01-13 09:22:54 +0000

    Add degrees celsius to the temperatures for both current temp and 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 const apiKey = '6e2fbb8ce6e558d699d84f8ca7aa2a98'
 
-
 function fetchWeather(search) {
   let queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=${apiKey}`;
   
@@ -30,61 +29,51 @@ function displayCurrentWeather(currentWeather) {
   const iconUrl = `https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}.png` 
   console.log(iconUrl)
   const icon = $('#icon').attr('src', iconUrl);
-  const temp = $('#temp').text(`Temperature: ${currentWeather.main.temp} C`);
+  const temp = $('#temp').text(`Temperature: ${currentWeather.main.temp} °C`);
   const wind = $('#wind').text(`Wind: ${currentWeather.wind.speed} kph`);
   const humidity = $('#humidity').text(`Humidity: ${currentWeather.main.humidity} %`);
 }
 
 function displayForecast(data) {
   const currentWeather = data.list;
-            displayCurrentWeather(data.list[0]);
+  displayCurrentWeather(data.list[0]);
             
-            const fiveDayForecast = currentWeather.filter(function (data) {
-              return data.dt_txt.includes('12:00:00');
-            });
-           console.log(fiveDayForecast);
+  const fiveDayForecast = currentWeather.filter(function (data) {
+    return data.dt_txt.includes('12:00:00');
+  });
+  console.log(fiveDayForecast);
 
-           $('#forecast').empty();
+  $('#forecast').empty();
 
-           for (let i = 0; i < fiveDayForecast.length; i++) {
-            const day = fiveDayForecast[i];
+  for (let i = 0; i < fiveDayForecast.length; i++) {
+    const day = fiveDayForecast[i];
+    const cardCol = $('<div>').attr('class', 'col-md');
+    const forecastCard = $('<div>').attr('class', 'card');
+    const forecastBody = $('<div>').attr('class', 'card-body');
+    const forecastIconUrl = `https://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
+    const forecastIcon = $('<img>').attr('src', forecastIconUrl);
+    const forecastTitle = $('<h5>').attr('class', 'card-title').text(dayjs(day.dt_txt).format('MM-DD-YYYY')); // Year optional
+    const forecastTemp = $('<p>').text(`Temp: ${day.main.temp} °C`);
+    const forecastWind = $('<p>').text(`Wind: ${day.wind.speed} kph`);
+    const forecastHumidity = $('<p>').text(`Humidity: ${day.main.humidity} %`);
 
-            const cardCol = $('<div>').attr('class', 'col-md');
-            const forecastCard = $('<div>').attr('class', 'card');
-            const forecastBody = $('<div>').attr('class', 'card-body');
-            const forecastIconUrl = `https://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
-            const forecastIcon = $('<img>').attr('src', forecastIconUrl);
-            const forecastTitle = $('<h5>').attr('class', 'card-title').text(dayjs(day.dt_txt).format('MM-DD-YYYY')); // Year optional
-            const forecastTemp = $('<p>').text(`Temp: ${day.main.temp} C`);
-            const forecastWind = $('<p>').text(`Wind: ${day.wind.speed} kph`);
-            const forecastHumidity = $('<p>').text(`Humidity: ${day.main.humidity} %`);
-
-            $('#forecast').append(cardCol);
-            cardCol.append(forecastCard);
-            forecastCard.append(forecastBody);
-            forecastBody.append(forecastTitle, forecastIcon, forecastTemp, forecastWind, forecastHumidity);
-
-         
-            
-           }
+    $('#forecast').append(cardCol);
+    cardCol.append(forecastCard);
+    forecastCard.append(forecastBody);
+    forecastBody.append(forecastTitle, forecastIcon, forecastTemp, forecastWind, forecastHumidity);
+  }
 }
 
-    
-    
+$('#search-button').on('click', function(e) {
+  e.preventDefault();
 
-    $('#search-button').on('click', function(e) {
-      e.preventDefault();
-
-      const search = $('#search-input').val().trim();
-      $('#today').attr('class', 'mt-3');
-      fetchWeather(search);
+  const search = $('#search-input').val().trim();
+  $('#today').attr('class', 'mt-3');
+  fetchWeather(search);
+})
 
 
-
-    })
-
-
-    // Movie button activity for persistant search (activity day 2 - 5 )
+    // Movie button activity for persistant search (activity day 2 - 5 ) - use localstorage
 
     // Style
 
